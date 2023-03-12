4d4ecbaaa8e24e73d4a4825751b66682136fed6b
rsingh859 <61348206+rsingh859@users.noreply.github.com>
2023-03-12 22:45:56 +0530

    Adding the program section

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,11 +1,12 @@
 import MainHeader from "../../molecules/MainHeader/MainHeader";
+import Programs from "../../molecules/Programs/Programs";
 import "./Home.css";
 
 function Home() {
   return (
     <div>
       <MainHeader />
-      Home
+      <Programs />
     </div>
   );
 }
